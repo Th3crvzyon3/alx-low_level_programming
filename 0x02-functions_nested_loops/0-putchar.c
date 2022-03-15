da11f7a6cf00d41055b79ede85adf3db364a6b78
Abraham Otieno <abrahamlincoln8212gmail.com>
2022-03-15 21:28:58 +0300

    Nested Loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,19 @@
-#include <stdio.h>
-#include <unistd.h>
-
+#include "main.h"
 /**
- * ft_putchar to print the characters given
- * main - Putchar
- * char c - The character given
- * Return: Always 0 (Success) 
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
-
-int ft_putchar(char c)
-{
-	write 1, &c, 8);
-	return (0);
-}
 
 int main(void)
 {
-	ft_putchar("_putchar\n");
+	int i = 1;
+	char name[8] = "_putchar";
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(name[i]);
+	}
+	_putchar('\n');
 	return (0);
 }
-
