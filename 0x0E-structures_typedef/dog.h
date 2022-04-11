f40cb4bd82a684cf3449a0f0a60051499d1b84a4
Abraham Otieno <abrahamlincoln8212gmail.com>
2022-04-12 01:49:54 +0300

    Structures

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,15 +1,25 @@
-#ifndef OURDOG
-#define OURDOG
+#ifndef THEDOG
+#define THEDOG
+
 /**
- * struct dog - Define a new struct dog with name, age, owner
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of dog
- **/
+  * struct dog - Short description
+  * @name: First member
+  * @age: Second member
+  * @owner: Third member
+  *
+  * Description: Longer description
+  * Tuwache kufork tu....by nixon
+  */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-#endif
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif /* the dog */
