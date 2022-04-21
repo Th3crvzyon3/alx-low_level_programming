ee7426781ca4c253abd06778ea63dbd0bbca8c86
Abraham Otieno <abrahamlincoln8212gmail.com>
2022-04-21 20:55:25 +0300

    List Lenght

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,16 +1,17 @@
 #include "lists.h"
-#include <stddef.h>
-#include <stdlib.h>
+
 /**
- * list_len - finds the number of nodes in a list
- * @h: singly linked list
- * Return: number of elements in a linked list
- */
+  * list_len - fills memory with a constant byte
+  * @h: is the owner of the dog
+  * Return: a number
+  */
 size_t list_len(const list_t *h)
 {
-	int i;
+	if (h == NULL)
+		return (0);
 
-	for (i = 0; h; i++)
-		h = h->next;
-	return (i);
+	if (h->next == NULL)
+		return (1);
+
+	return (1 + list_len(h->next));
 }
